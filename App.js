4da75768341d31218c03a65350a075e494a93367
Deep Vyas <deepvyas2003@gmail.com>
2023-12-07 19:38:41 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import AppNavigation from './navigation/appNavigation';
+import { DrawerNavigation } from './navigation/appNavigation'
+import { NavigationContainer } from '@react-navigation/native';
 
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import HomeScreen from './screens/HomeScreen';
-// import GameStore from './screens/gameStore';
-
-// const Tab = createBottomTabNavigator();
 
 
 export default function App() {
   return (
-    <AppNavigation />
+    
+       <DrawerNavigation />
+  
   );
 }
