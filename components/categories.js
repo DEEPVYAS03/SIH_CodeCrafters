81edf3ce17aa85e074500e97c688f175c21f42f9
Deep Vyas <deepvyas2003@gmail.com>
2023-12-16 20:17:44 +0530

    Card render

--- components/categories.js
+++ components/categories.js
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { View, Text, TextInput, ScrollView, TouchableOpacity ,Image } from "react-native";
 import tw from "twrnc";
-import { categories } from '../constants';
+// import { categories } from '../constants';
 import { useNavigation } from "@react-navigation/native";
+import axios from 'axios'
+import { categories } from './../constants/index';
 
 export default function Categories({ item }) {
     const { restaurants } = item;
-    console.log('restaurants:', restaurants)
-    const [activeCategory, setActiveCategory] = useState(null);
-    const navigation = useNavigation();
+    // console.log('restaurants:', restaurants)
 
+
+    const [activeCategory, setActiveCategory] = useState(null);
+    const [categories,setCategories] = useState([])
+    const navigation = useNavigation();
+    const projectcat = async () => {
+        try {
+          // Make a GET request
+          const response = await axios.get('https://sih-backend.vercel.app/api/user/stateWiseProjects');
+          console.log('GET Response:', response.data);
+          setCategories(response.data.states)
+        } catch (error) {
+          console.error('GET Error:', error);
+        }
+        // console.log(asd)
+      };
+     
+      useEffect(() => {
+        projectcat();
+      }, [])    
+     
     return (
         <View style={tw`mt-4`}>
             <ScrollView
--- constants/index.js
+++ constants/index.js
@@ -1,42 +1,80 @@
-export const categories = [
-    {
-        id: 1,
-        name: 'Andra Pradesh',
-        image: require('../assets/images/pizzaIcon.png'),
-    },
-    {
-        id: 2,
-        name: 'Tamil Nadu',
-        image: require('../assets/images/pizzaIcon.png'),
-    },
-    {
-        id: 3,
-        name: 'Maharashtra',
-        image: require('../assets/images/pizzaIcon.png'),
-    },
-    {
-        id: 4,
-        name: 'UP',
-        image: require('../assets/images/pizzaIcon.png'),
-    },
-    {
-        id: 5,
-        name: 'Bihar',
-        image: require('../assets/images/pizzaIcon.png'),
-    },
-    {
-        id: 6,
-        name: 'Gujarat',
-        image: require('../assets/images/pizzaIcon.png'),
-    },
+import { useEffect } from 'react';
+import axios from 'axios'
+import Categories from './../components/categories';
 
-]
+
+let response;
+let categories=[];
+
+const projects = async () => {
+   try {
+     // Make a GET request
+      response = await axios.get('https://sih-backend.vercel.app/api/user/stateWiseProjects');
+      categories = response.data.states
+      return categories;
+
+   //   console.log('GET Response:', response.data);
+   } catch (error) {
+     console.error('GET Error:', error);
+   }
+
+   
+ };
+
+export {categories}
+ 
+
+
+
+
+
+
+    
+
+
+
+
+
+
+// export const categories = [
+//     {
+//         id: 1,
+//         name: 'Andra Pradesh',
+//         image: require('../assets/images/pizzaIcon.png'),
+//     },
+//     {
+//         id: 2,
+//         name: 'Tamil Nadu',
+//         image: require('../assets/images/pizzaIcon.png'),
+//     },
+//     {
+//         id: 3,
+//         name: 'Maharashtra',
+//         image: require('../assets/images/pizzaIcon.png'),
+//     },
+//     {
+//         id: 4,
+//         name: 'UP',
+//         image: require('../assets/images/pizzaIcon.png'),
+//     },
+//     {
+//         id: 5,
+//         name: 'Bihar',
+//         image: require('../assets/images/pizzaIcon.png'),
+//     },
+//     {
+//         id: 6,
+//         name: 'Gujarat',
+//         image: require('../assets/images/pizzaIcon.png'),
+//     },
+
+// ]
 
 
 export const featured = {
     restaurants: [
         {
-            id: 1,//categories .name
+            id: 1,
             image: require('../assets/images/pizza.png'),
             description: 'Hot and spicy pizzas',
             lng: -85.5324269,
@@ -44,21 +82,21 @@ export const featured = {
             dishes: [
                 {
                    id: 1,
-                   name: 'pizza',
+                   name: 'WDC1',
                    description: 'cheezy garlic pizza',
                    price: 10,
                    image:  require('../assets/images/pizzaDish.png')
                 },
                 {
                    id: 2,
-                   name: 'pizza',
+                   name: 'WDC2',
                    description: 'cheezy garlic pizza',
                    price: 10,
                    image:  require('../assets/images/pizzaDish.png')
                 },
                 {
                    id: 3,
-                   name: 'pizza',
+                   name: 'WDC3',
                    description: 'cheezy garlic pizza',
                    price: 10,
                    image:  require('../assets/images/pizzaDish.png')
@@ -66,17 +104,16 @@ export const featured = {
             ]
     
         },
+
+
+
         {
             id: 2,
             name: 'Papa Johns',
             image: require('../assets/images/pizza.png'),
-            description: 'Hot and spicy pizzas',
+            description: 'IBIBIBWS',
             lng: -85.5324269,
             lat: 38.2145602,
-            address: '434 second street',
-            stars: 4,
-            reviews: '4.4k',
-            category: 'Fast Food',
             dishes: [
                 {
                    id: 1,
